e945944682787f975c71c86e65e0def4e63884dc
sim <sim30217@gmail.com>
2023-12-03 18:41:52 +0900

    lambda

--- src/brace_initialization.cpp
+++ src/brace_initialization.cpp
@@ -8,9 +8,24 @@ public:
 private:
 	int k_;
 };
-int main() {
-double x = 2.58;		// Assigned or input somewhere
-SimpleClass sc_01(x);
-SimpleClass sc_02{x};
 
+enum OptionType
+{
+    European,     	// default integer value = 0
+    American,     	// default integer value = 1
+    Bermudan,     	// default integer value = 2
+    Asian	      	// default integer value = 3
+};
+
+#include <iostream>
+
+int main() {
+	double x = 2.58;    // Assigned or input somewhere
+	SimpleClass sc_01(x);
+	SimpleClass sc_02{x};
+	std::cout << " European = " << European << std::endl;
+	std::cout << " American = " << American << std::endl;
+	std::cout << " Bermudan = " << Bermudan << std::endl;
+	std::cout << " Asian = " << Asian << std::endl;
+	std::cout << std::endl;
 }
